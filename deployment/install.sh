f59c002aa8f97d7855308c7d0e050c2d6a90bc98
DeV1doR <bogdankurinniy.dev1@gmail.com>
2017-04-19 16:16:58 +0000

    update some install scripts

--- deployment/install.sh
+++ deployment/install.sh
@@ -8,12 +8,19 @@ fi
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/vars.sh
 
+sudo apt-get install libmysqlclient-dev python-dev mysql-server-5.6 default-jre zookeeperd
+
+mkdir -p ~/Downloads
+wget "http://mirror.cc.columbia.edu/pub/software/apache/kafka/0.10.1.0/kafka_2.10-0.10.1.0.tgz" -O ~/Downloads/kafka.tgz
+mkdir -p ~/kafka && cd ~/kafka
+tar -xvzf ~/Downloads/kafka.tgz --strip 1
 
 # GGOOOO
 
 # get godep lib
 go get -u github.com/tools/godep
 # install plugins from godep dir
+cd $GAME_APP_DIR
 godep restore
 
 
@@ -31,3 +38,5 @@ python $DJANGO_APP_DIR/manage.py migrate
 cd $DJANGO_APP_DIR
 npm install
 npm run exec_only
+
+./manage.py collectstatic --noinput
