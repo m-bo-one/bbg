c8b2290ba72a2d864310f137c3ecb43b228d4078
devidor <devidor@syneforge.com>
2017-04-20 11:43:27 +0300

    fix runner

--- deployment/run.sh
+++ deployment/run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/vars.sh
+source $DIR/start_kafka.sh
+
+sleep 2
 
 tmux new-session -d -s $PROJECT
 tmux attach-session $PROJECT
